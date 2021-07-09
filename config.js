1eb796e1ca732f4c0cb5193d18f8b65f86268f08
Joe Sabbagh <joesabbagh12345@gmail.com>
2021-07-09 03:08:36 +0300

    1

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'jNrh2lErqHQ8X5c7aNn2gMQQt',
-    consumer_secret: 'WR80CIFkXDsPcA4beK0QmOGajL97bpugf1y1xK7nSUqEsHViWg',
-    access_token: '1412360140737626112-oyCvcpZ89gaH1AENXNVP57FNbdObIJ',
-    access_token_secret: 'EernhhwieAnb5fyEkBctuEXZa8ooHbNpfvte7fkcJutAZ',
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: '',
 }
