c1e0a2ca5d2f88f128ab843f037a7b8477f1d516
Joe Sabbagh <joesabbagh12345@gmail.com>
2021-07-10 01:48:03 +0300

    Final code without pagination

--- index.js
+++ index.js
@@ -3,19 +3,10 @@ var config = require('./config');
 var fs = require('fs');
 var T = new Twit(config);
 
-var params = {
-    q: '#FreeFromHijab',
-    count: 100
-};
-
-T.get('search/tweets', params, gotData);
-
-function gotData(err, data, response) {
-
+//first page
+T.get('search/tweets', { q: '#FreeFromHijab', count: 100 }, function gotData(err, data, response) {
     var tweets = data.statuses;
-
-    var data = {};
-    data.table = [];
+    var last_id = tweets[tweets.length - 1].id_str;
 
     for (let i = 0; i < tweets.length; i++) {
         if (typeof tweets[i].entities.media != "undefined") {
@@ -27,32 +18,15 @@ function gotData(err, data, response) {
             for (let j = 0; j < tweets[i].extended_entities.media.length; j++) {
                 console.log(tweets[i].extended_entities.media[j].media_url);
             }
-
             console.log("\n");
+        }
+    }
 
-            // let media_arr = [];
-            // for (let j = 0; j < tweets[i].extended_entities.media.length; j++) {
-            //     media_arr[j] = tweets[i].extended_entities.media[j].media_url;
-            // }
-            // var obj = {
-            //     date: tweets[i].created_at,
-            //     user_name: tweets[i].user.name,
-            //     text: tweets[i].text,
-            //     media: [],
-            // }
-            // obj['media'].push(media_arr);
-            // data.table.push(obj)
-
-            // fs.writeFile("input.json", JSON.stringify(data, null, 2), function (err) {
-            //     if (err) throw err;
-            //     console.log('complete');
-            // }
-            // );
-
-            fs.readFile('data.json', function (err, content) {
-                if (err) throw err;
-                var parseJson = JSON.parse(content);
-
+    fs.readFile('data.json', function (err, content) {
+        if (err) throw err;
+        var parseJson = JSON.parse(content);
+        for (let i = 0; i < tweets.length; i++) {
+            if (typeof tweets[i].entities.media != "undefined") {
                 let media_arr = [];
                 for (let j = 0; j < tweets[i].extended_entities.media.length; j++) {
                     media_arr[j] = tweets[i].extended_entities.media[j].media_url;
@@ -64,13 +38,57 @@ function gotData(err, data, response) {
                     media: [],
                 }
                 obj['media'].push(media_arr);
-                parseJson.table.push(obj);
-
-                fs.writeFile('data.json', JSON.stringify(parseJson, null, 2), function (err) {
-                    if (err) throw err;
-                })
-            })
+                parseJson.table.push(obj)
+            }
         }
-    }
-}
+
+        fs.writeFile('data.json', JSON.stringify(parseJson, null, 2), function (err) {
+            if (err) throw err;
+        })
+    })
+
+    //the id of the last tweet we used
+
+    //Next page
+    // T.get('search/tweets', { q: '#FreeFromHijab', count: 200, last_id: last_id }, function gotData(err, data, response) {
+    //     for (let i = 0; i < tweets.length; i++) {
+    //         if (typeof tweets[i].entities.media != "undefined") {
+
+    //             console.log(tweets[i].created_at);
+    //             console.log(tweets[i].user.name);
+    //             console.log(tweets[i].text);
+
+    //             for (let j = 0; j < tweets[i].extended_entities.media.length; j++) {
+    //                 console.log(tweets[i].extended_entities.media[j].media_url);
+    //             }
+    //             console.log("\n");
+    //         }
+    //     }
+
+    //     fs.readFile('data.json', function (err, content) {
+    //         if (err) throw err;
+    //         var parseJson = JSON.parse(content);
+    //         for (let i = 0; i < tweets.length; i++) {
+    //             if (typeof tweets[i].entities.media != "undefined") {
+    //                 let media_arr = [];
+    //                 for (let j = 0; j < tweets[i].extended_entities.media.length; j++) {
+    //                     media_arr[j] = tweets[i].extended_entities.media[j].media_url;
+    //                 }
+    //                 var obj = {
+    //                     date: tweets[i].created_at,
+    //                     user_name: tweets[i].user.name,
+    //                     text: tweets[i].text,
+    //                     media: [],
+    //                 }
+    //                 obj['media'].push(media_arr);
+    //                 parseJson.table.push(obj)
+    //             }
+    //         }
+
+    //         fs.writeFile('data.json', JSON.stringify(parseJson, null, 2), function (err) {
+    //             if (err) throw err;
+    //         })
+    //     })
+    // })
+})
 
